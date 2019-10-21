182d18a651f419ba8377a6380b46d0a55aeebca3
Axel Jonckheere <axel.jonckheere@endare.com>
2019-10-21 11:27:24 +0200

    Snackbar notifications are now queued and shown one by one

--- app/src/main/java/com/endare/adhese/sdksample/SnackbarManager.java
+++ app/src/main/java/com/endare/adhese/sdksample/SnackbarManager.java
@@ -3,14 +3,20 @@ package com.endare.adhese.sdksample;
 import android.content.Context;
 import android.view.View;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import androidx.annotation.NonNull;
 
 public class SnackbarManager {
 
     private View rootLayout;
+    private boolean isDisplayingSnackbar;
 
+    private final Queue<Snackbar> snackbarQueue;
     private final int colorInfo;
     private final int colorSuccess;
     private final int colorError;
@@ -22,6 +28,7 @@ public class SnackbarManager {
         this.colorSuccess = context.getResources().getColor(R.color.colorSuccess);
         this.colorError = context.getResources().getColor(R.color.colorError);
         this.colorText = context.getResources().getColor(android.R.color.white);
+        this.snackbarQueue = new LinkedList<>();
     }
 
     public void setRootLayout(@NonNull View rootLayout) {
@@ -40,11 +47,48 @@ public class SnackbarManager {
         show(message, colorError);
     }
 
+    /**
+     * Calling this method will queue the Snackbar and show it when it's its turn in the queue.
+     */
     private void show(@NonNull String message, int backgroundColor) {
         Snackbar snackbar = Snackbar.make(rootLayout, message, Snackbar.LENGTH_LONG)
-                .setActionTextColor(colorText);
+                .setActionTextColor(colorText)
+                .setDuration(BaseTransientBottomBar.LENGTH_LONG)
+                .addCallback(new Snackbar.Callback() {
+
+                    @Override
+                    public void onDismissed(Snackbar transientBottomBar, int event) {
+
+                        if (snackbarQueue.size() > 0 && event != Snackbar.Callback.DISMISS_EVENT_CONSECUTIVE) {
+                            showNext();
+                            return;
+                        }
+
+                        isDisplayingSnackbar = false;
+                    }
+                });
+
         snackbar.getView().setBackgroundColor(backgroundColor);
-        snackbar.show();
+
+        snackbarQueue.add(snackbar);
+
+        if (!isDisplayingSnackbar) {
+            showNext();
+        }
+    }
+
+    /**
+     * Shows the next Snackbar in the queue
+     */
+    private void showNext() {
+        Snackbar nextSnackbar = snackbarQueue.poll();
+
+        if (nextSnackbar == null) {
+            return;
+        }
+
+        nextSnackbar.show();
+        isDisplayingSnackbar = true;
     }
 
 }
